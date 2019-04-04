8b68f0553d2092bbc718ca5e22e6d095f06c521a
ssoundarya2010 <ssoundarya2010@gmail.com>
2019-04-04 11:25:30 +0000

    new

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,20 +5,38 @@ sap.ui.define([
 
 	return Controller.extend("report.ProdReport.controller.View1", {
 		onInit: function () {
-			 $.ajax({
+			//  $.ajax({
 
-				method: "GET",
+			// 	method: "GET",
+			// 	url: "/DEVPortal",
+			// 	headers: {
+			// 		"X-DBP-APIKEY": "fb0BtZAYPigSPfv1K3TwYOlLUOGLxdqA"
+			// 	},
+
+			// 	success: function (oData) {
+
+			// 		return oData;
+
+			// 	},
+			// 	error: function (oError) {
+
+			// 	}
+
+			// });
+			jQuery.ajax({
+
 				url: "/DEVPortal",
+				//	url: "https://DBGAPIManagement.apimanagement.hana.ondemand.com:443/prod/prisma-margin-estimator/1.0.0/products",
 				headers: {
 					"X-DBP-APIKEY": "fb0BtZAYPigSPfv1K3TwYOlLUOGLxdqA"
 				},
-
-				success: function (oData) {
-
-					return oData;
-
+				async: false,
+				method: "GET",
+				dataType: "json",
+				success: function (data) {
+					
 				},
-				error: function (oError) {
+				error: function (error) {
 
 				}
 
